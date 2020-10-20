251e869020eff4d2d253c11a7ec52bf02e1b9d8f
Guillaume-TR <guillaume.terrien.gt@gmail.com>
2020-10-20 12:21:50 +0200

    resolved: map in the home is not displayed

--- web/src/components/Home/Map.js
+++ web/src/components/Home/Map.js
@@ -9,7 +9,7 @@ import {
 class HomeMap extends React.Component {
   render() {
     const accessToken = 'pk.eyJ1IjoiZGFlbmVyeXM5NSIsImEiOiJjazJmYjNlN2QwZ3luM2xwYnlqZnE5Z3JmIn0.0eaxPyVL6cJ0QxnXXP_fHg';
-    const url = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+    const url = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
     const creators = [
       {
         id: 1,
@@ -53,7 +53,7 @@ class HomeMap extends React.Component {
         <div className="homemap-content">
           <LeafletMap
             center={[46.603354, 1.8883335]}
-            zoom={5}
+            zoom={6}
             maxZoom={18}
             attributionControl
             zoomControl
@@ -65,7 +65,9 @@ class HomeMap extends React.Component {
           >
             <TileLayer
               url={url}
-              id="mapbox.streets"
+              tileSize={512}
+              zoomOffset={-1}
+              id="mapbox/streets-v11"
               accessToken={accessToken}
             />
             { creators.map((creator) => (
